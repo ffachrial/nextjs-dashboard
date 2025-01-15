300a69090fadab7fe65b2bb568b2dc2cbcfe46ed
ffachrial <19848938+ffachrial@users.noreply.github.com>
2025-01-15 12:35:11 +0700

    Fixing font, Error handling, and remark for auth config

--- auth.config.ts
+++ auth.config.ts
@@ -17,5 +17,8 @@ export const authConfig = {
       return true;
     },
   },
-  providers: [],
+  providers: [
+    // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
+    // while this file is also used in non-Node.js environments
+  ],
 } satisfies NextAuthConfig;
