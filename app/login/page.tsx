55cfd15b832d0a33041fc7c39403cb5930610b86
ffachrial <19848938+ffachrial@users.noreply.github.com>
2025-02-06 20:31:22 +0700

    Change login header

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,5 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+import KV2Logo from '../ui/kv2-logo';
 import LoginForm from '@/app/ui/login-form';
 import { Metadata } from 'next';
 
@@ -10,9 +11,10 @@ export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+        <div className="flex h-20 w-full items-end rounded-lg bg-yellow-300 p-3 md:h-36">
           <div className="w-32 text-white md:w-36">
-            <AcmeLogo />
+            <KV2Logo />
+            {/* <AcmeLogo /> */}
           </div>
         </div>
         <LoginForm />
